159d8c8ffa43ec4879e3674ff1bcd09b7c2dec4f
Aman Lath <amanlath643@gmail.com>
2023-06-12 12:43:56 +0000

    operator.js Updated

--- operator.js
+++ operator.js
@@ -1,8 +1,15 @@
+//addition
 const My_Var = 5 + 10;
 console.log(My_Var);
 
+//substraction
 const myVar = 12 - 6;
 console.log(myVar);
 
+//Multiplication
 const mul = 13 * 13;
 console.log(mul);
+
+//Division
+const div =4 / 2;
+console.log(div);
