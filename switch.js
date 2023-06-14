3a548b0776a3f68b257e10aa79d3438ca0a8a889
Aman Lath <amanlath643@gmail.com>
2023-06-14 16:25:25 +0000

    switch.js created

---
+++ switch.js
@@ -0,0 +1,12 @@
+const num = 3;
+
+switch (num) {
+    case 1:
+        console.log("Value is 1")
+        break;
+    case 2:
+        console.log("Value is 2");
+        break;
+    default:
+        console.log("Case doesn\'t matched");
+}
