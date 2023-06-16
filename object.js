c67f90adc85fa5117103d43c7caf0abcddce5d64
Aman Lath <amanlath643@gmail.com>
2023-06-16 11:26:40 +0000

    object.ja created

---
+++ object.js
@@ -0,0 +1,9 @@
+const cat = {
+    "name": "Aman Lath",
+    "age": 19
+};
+
+const name = cat.name;
+const age = cat.age;
+console.log(name);
+console.log(age);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+name;
+const age = cat.age;
